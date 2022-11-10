8b5b60d301b0c9fbe762892151980b4045fc8644
Jake Martin <jm9357481@gmail.com>
2022-11-10 15:31:06 -0800

    disable CORS changes

--- audit/app.py
+++ audit/app.py
@@ -9,6 +9,7 @@ import logging
 import logging.config
 import json
 from pykafka import KafkaClient
+from flask_cors import CORS, cross_origin
 
 # load app config
 with open("app_config.yml", "r") as f:
@@ -74,6 +75,8 @@ def get_question(index):
 
 
 app = connexion.FlaskApp(__name__, specification_dir="")
+CORS(app.app)
+app.app.config['CORS_HEADERS'] = 'Content-Type'
 app.add_api("openapi.yml", strict_validation=True, validate_responses=True)
 
 if __name__ == "__main__":
--- processing/app.py
+++ processing/app.py
@@ -5,6 +5,7 @@ import connexion
 from connexion import NoContent
 import swagger_ui_bundle
 import requests
+from flask_cors import CORS, cross_origin
 from apscheduler.schedulers.background import BackgroundScheduler
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -198,6 +199,8 @@ def init_scheduler():
 
 
 app = connexion.FlaskApp(__name__, specification_dir="")
+CORS(app.app)
+app.app.config['CORS_HEADERS'] = 'Content-Type'
 app.add_api("openapi.yml", strict_validation=True, validate_responses=True)
 
 if __name__ == "__main__":
